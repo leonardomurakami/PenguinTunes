d3d952b98198b616be87de3596001c24527055a0
Leonardo Murakami <contato@murakams.com>
2024-03-31 15:55:01 -0300

    [feat] make default search spotify

--- modules/cogs/music.py
+++ modules/cogs/music.py
@@ -185,10 +185,12 @@ class Music(commands.Cog):
             )
         elif query.startswith("ytsearch:") or query.startswith("yt:"):
             tracks: wavelink.Search = await wavelink.Playable.search(
-                query.split(":", 1)[1], source="spsearch:"
+                query.split(":", 1)[1], source="ytsearch:"
             )
         else:
-            tracks: wavelink.Search = await wavelink.Playable.search(query)
+            tracks: wavelink.Search = await wavelink.Playable.search(
+                query.split(":", 1)[1], source="spsearch:"
+            )
         if not tracks:
             await ctx.send(
                 f"{ctx.author.mention} - Could not find any tracks with that query. Please try again."
